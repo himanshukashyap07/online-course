6271a12eea9d6fe576659d8f859de4aa629e4faf
himanshu kashyap <kashyapji1908@gmail.com>
2025-10-27 08:49:18 +0530

    forgotPassword route

--- src/app/api/(User)/forgotPassword/route.ts
+++ src/app/api/(User)/forgotPassword/route.ts
@@ -1,7 +1,47 @@
+import dbConnect from "@/lib/dbConnect";
+import User from "@/models/user";
+import apiError from "@/utils/apiError";
+import apiResponse from "@/utils/apirespone";
+import { getServerSession } from "next-auth";
 import { NextRequest } from "next/server";
+import { authOption } from "../../(Auth)/auth/[...nextauth]/options";
+import bcrypt from "bcryptjs";
 
 
 
-export async function PATCH(req:NextRequest){
-    const {oldPassword,newPasword} = await req.json()
+export async function PATCH(req: NextRequest) {
+    const { oldPassword, newPasword } = await req.json()
+    const sesssion = await getServerSession(authOption)
+    try {
+
+        if (!oldPassword || !newPasword) {
+            return apiError("password and email is required")
+        }
+        if (!sesssion) {
+            return apiError("You must be logged in to update password", 401)
+        }
+        await dbConnect()
+        const email = sesssion.user?.email as string;
+        const user = sesssion.user as any;
+        const ispasswordCorrect = bcrypt.compare(oldPassword, user.password);
+        if (!ispasswordCorrect) {
+            return apiError("old password is incorrect", 400)
+        }
+
+        const updatedUser = await User.findOneAndUpdate({ email }, {
+            $set: {
+                password: newPasword
+            }
+        }, {
+            new: true
+        })
+        if (!updatedUser) {
+            return apiError("user not found", 404)
+        }
+
+        return apiResponse("user password updated successfully", 200)
+
+    } catch (error) {
+        return apiError("internal server error in updating password")
+    }
 }
--- src/app/api/(User)/updateUser/route.ts
+++ src/app/api/(User)/updateUser/route.ts
@@ -1,7 +1,17 @@
-import { NextRequest } from "next/server";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { authOption } from "../../(Auth)/auth/[...nextauth]/options";
+import apiError from "@/utils/apiError";
 
 
 
-export async function PUT(req:NextRequest){
+export async function GET(){
+ const session = await getServerSession(authOption);
+    if(!session){
+        return apiError("You must be logged in to update user details",401);
+    }
+    
+    console.log(session);
+    return NextResponse.json({message:"User is logged in",user:session.user});
 
 }
